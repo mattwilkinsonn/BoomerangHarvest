41e74da7ac24a95cd7073153b4664031fef02cc8
Matt Wilkinson <matt@mattwilkinson.dev>
2023-01-08 14:49:49 -0500

    gameover
    Signed-off-by: Matt Wilkinson <matt@mattwilkinson.dev>

--- Menus/GameOverMenu.gd
+++ Menus/GameOverMenu.gd
@@ -4,9 +4,24 @@ signal play_again
 
 var score: int = 0
 
+@export var GOOD_LINE = "Walpurgis will reward you..."
+@export var GOOD_AMOUNT = 25
+@export var AVERAGE_LINE = "Walpurgis is satisfied"
+@export var AVERAGE_AMOUNT = 10
+@export var BAD_LINE = "Walpurgis is not happy"
+
 func init(player_score: int):
 	score = player_score
-	$VBoxContainer/Score.text = 'Score: ' + str(score)
+	$VBoxContainer/VBoxContainer/Score.text = 'Harvested: ' + str(score)
+	$VBoxContainer/VBoxContainer/Walpurgis.text = get_line(score)
+
+func get_line(score: int):
+	if score < AVERAGE_AMOUNT:
+		return BAD_LINE
+	if score < GOOD_AMOUNT:
+		return AVERAGE_LINE
+	
+	return GOOD_LINE
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
