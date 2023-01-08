b3dda38ec0d75aebc32f4bde570c375048b38907
Matt Wilkinson <matt@mattwilkinson.dev>
2023-01-08 15:59:14 -0500

    adjusting plant sizes
    Signed-off-by: Matt Wilkinson <matt@mattwilkinson.dev>

--- Game/Plant/Enemy/Bomb.gd
+++ Game/Plant/Enemy/Bomb.gd
@@ -1,16 +1,11 @@
 extends StaticBody2D
 
-@export var COLOR: Color = Color.RED
-@export var EXPLOSION_COLOR: Color = Color.ORANGE
 @export var LIFE_TIME = 3.0
 
 const PlantDeathScene = preload("res://Game/Plant/PlantDeath.tscn")
 const BombExplosionScene = preload("res://Game/Plant/Enemy/BombExplosion.tscn")
 
 var player
-
-func _draw():
-	draw_circle(Vector2.ZERO, $CollisionShape2D.shape.radius, COLOR)
 
 func _ready():
 	player = get_node("../Player")
